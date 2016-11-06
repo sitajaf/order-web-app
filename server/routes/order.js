8de5715fd3caf6de3772ace07cdf480bafdb30f4
Jafari Sitakange <jafari.sitakange@complispace.com.au>
2016-11-06 15:19:38 +1100

    save received order in elastic search

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const ElasticSearchService = require('../services/elasticService');
+
+const elasticSearchService = new ElasticSearchService();
 
 router.get('/order', (req, res) => {
     res.send({
@@ -6,5 +9,21 @@ router.get('/order', (req, res) => {
         name: 'Kawunga'
     })
 });
+
+router.post('/order', (req, res) => {
+    const order = req.body;
+    if (valid(order)) {
+        elasticSearchService.add('order', order)
+            .then((id)=> {
+                res.status(201).send('Order Saved!');
+            },(err)=>{
+                res.status(500).send({message: 'Error while saving Order!'})
+            });
+    }
+});
+
+const valid = (order) => {
+    return order !== undefined && order !== {};
+};
 
 module.exports = router;
