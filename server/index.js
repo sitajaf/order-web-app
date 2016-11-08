84a323f4f533c8aa42a9209d1420e8eb401874dd
Jafari Sitakange <jafari.sitakange@complispace.com.au>
2016-11-08 23:56:06 +1100

    allow metods in access control

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
 });
 app.use(bodyParser.json());
